446700794b694b693a74808c2350ba41a56e4617
nlloyd1971 <40879623+nlloyd1971@users.noreply.github.com>
2023-03-04 11:11:57 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -a
+
+set -euo pipefail
+
+wf kubeconfig --cluster ${CLUSTER_NAME} -w ${WAYFINDER_WORKSPACE}
+
+mkdir -p /tmp/manifests
+cp -fR /source/manifests/* /tmp/manifests/
+cat <<EOF >/tmp/manifests/kustomization.yaml
+resources:
+- deployment.yaml
+- ingress.yaml
+images:
+- name: application
+  newName: ghcr.io/appvia/application
+  newTag: ${IMAGE_TAG}
+patches:
+  - target:
+      kind: Ingress
+      name: application-ingress
+    patch: |-
+      - op: replace
+        path: /spec/rules/0/host
+        value: main.${DNS_ZONE}
+      - op: replace
+        path: /spec/tls/0/hosts/0
+        value: main.${DNS_ZONE}
+EOF
+
+echo "Getting pods"
+kubectl get pods -n ${NAMESPACE_NAME}
+
+echo "Applying manifests"
+kubectl -n ${NAMESPACE_NAME} apply --v=6 -k /tmp/manifests

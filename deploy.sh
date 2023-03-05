e31ae47ef45128d0da2f029da525a9716abe7166
nlloyd1971 <40879623+nlloyd1971@users.noreply.github.com>
2023-03-05 16:51:58 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 wf kubeconfig --cluster ${CLUSTER_NAME} -w ${WAYFINDER_WORKSPACE}
 
 mkdir -p /tmp/manifests
-cp -fR /source/manifests/deployment.yaml /tmp/manifests/deployment.yaml
+#cp -fR /source/manifests/deployment.yaml /tmp/manifests/deployment.yaml
 #cp -fR /source/manifests/* /tmp/manifests/ - IS THIS IN THE NEW IMAGE
 cat <<EOF >/tmp/manifests/kustomization.yaml
 resources:
@@ -31,5 +31,5 @@ EOF
 echo "Getting pods"
 kubectl get pods -n ${NAMESPACE_NAME}
 
-echo "Applying manifests"
-kubectl -n ${NAMESPACE_NAME} apply --v=6 -k /tmp/manifests
+#echo "Applying manifests"
+#kubectl -n ${NAMESPACE_NAME} apply --v=6 -k /tmp/manifests

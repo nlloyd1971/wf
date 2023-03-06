07ae5636e7c2015aa1c7ee9d0b5f47d5ee52379d
nlloyd1971 <40879623+nlloyd1971@users.noreply.github.com>
2023-03-06 12:09:20 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,31 +7,19 @@ wf kubeconfig --cluster ${CLUSTER_NAME} -w ${WAYFINDER_WORKSPACE}
 mkdir -p /tmp/manifests
 #cp -fR /source/manifests/deployment.yaml /tmp/manifests/deployment.yaml
 #cp -fR /source/manifests/* /tmp/manifests/ - IS THIS IN THE NEW IMAGE
-cat <<EOF >/tmp/manifests/kustomization.yaml
+cat <<EOF >/home/kustomization.yaml
 resources:
 - deployment.yaml
-- ingress.yaml
 images:
 - name: application
-  newName: ghcr.io/appvia/application
+  newName: ghcr.io/nlloyd1971/wf
   newTag: ${IMAGE_TAG}
-patches:
-  - target:
-      kind: Ingress
-      name: application-ingress
-    patch: |-
-      - op: replace
-        path: /spec/rules/0/host
-        value: main.${DNS_ZONE}
-      - op: replace
-        path: /spec/tls/0/hosts/0
-        value: main.${DNS_ZONE}
 EOF
 
 echo "Getting pods"
 kubectl get pods -n ${NAMESPACE_NAME}
 
 #echo "Applying manifests"
-#kubectl -n ${NAMESPACE_NAME} apply --v=6 -k /tmp/manifests
+kubectl -n ${NAMESPACE_NAME} apply --v=6 -k /home
 
-kubectl -n ${NAMESPACE_NAME} apply --v=6 -f /home/deployment.yaml
+#kubectl -n ${NAMESPACE_NAME} apply --v=6 -f /home/deployment.yaml
